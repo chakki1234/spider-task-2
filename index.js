01023e27f8fa8de67f0d31d9810cfd99ec2b746d
chakki1234 <chakkirala.a@gmail.com>
2019-06-04 22:43:12 +0530

    normal mode almost

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var right = document.querySelector("#right");
 var left = document.querySelector("#left");
 var start = document.querySelector("#start");
 var stop = document.querySelector("#stop");
-
+var playagain = document.querySelector("#playagain");
+var scorecount = 0;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight-150;
@@ -15,6 +16,8 @@ var radiussmall = 10;
 var xc = canvas.width/2
 var yc = canvas.height-100;
 var i = 0;
+
+var repeat = 0;
 
 var angle=Math.PI/180;
 var redx = xc+radius;
@@ -31,15 +34,22 @@ var sma = canvas.height;
         	listblock[k] = new Object();
 
         	listblock[k].y = 0-150*k;
-        	listblock[k].dy = 0.2;
-        	listblock[k].width = Math.random()*100;
-        	listblock[k].height = Math.random()*100;
-        	listblock[k].x = canvas.width/2;
+        	listblock[k].dy = 0.5;0
+        	listblock[k].width = Math.random()*150;
+        	listblock[k].height = Math.random()*25 + 25;
+        	listblock[k].x = Math.random()*200 + canvas.width/2 - 150;
     
        }
 
 var play = 1;
 var touched = 0;
+
+
+
+playagain.addEventListener("click", function(){
+ document.location.reload();
+});
+
 
 start.addEventListener("click", function(){
  play = 1;
@@ -52,7 +62,7 @@ play = 0;
 
 
 right.addEventListener("click" , function(){
-	angle+=(7*Math.PI/180);
+	angle+=(20*Math.PI/180);
         redx = xc+Math.cos(angle)*radius;
         redy = yc+Math.sin(angle)*radius;
         bluex = xc+Math.cos(Math.PI+angle)*radius;
@@ -63,7 +73,7 @@ right.addEventListener("click" , function(){
 
 left.addEventListener("click", function(){
    
-   angle-=(7*Math.PI/180);
+   angle-=(20*Math.PI/180);
    redx = xc+Math.cos(angle)*radius;
    redy = yc+Math.sin(angle)*radius;
    bluex = xc+Math.cos(Math.PI+angle)*radius;
@@ -104,55 +114,50 @@ function findsmall(){
                   clearInterval(interval);
                   */}
 
-                /* else 
-
-               {*/
-                 	if(bluex>=listblock[r].x-radiussmall && bluex<=listblock[r].x+listblock[r].width+radiussmall && bluey>=listblock[r].y && bluey<=listblock[r].y+listblock[r].height)
+                 else if(bluex>=listblock[r].x-radiussmall && bluex<=listblock[r].x+listblock[r].width+radiussmall && bluey>=listblock[r].y && bluey<=listblock[r].y+listblock[r].height)
                
                 {
                  console.log("game over");
                   touched = 1;
-                /* break;
-                 alert("game over");
-               document.location.reload();
-                 clearInterval(interval);
-                 */
                  }
 
 
-             /*    else 
-               {
-                  if(redy>=listblock[r].y-radiussmall && redy<=listblock[r].y+listblock[r].height+radiussmall && redx>=listblock[r].x && redx<=listblock[r].x+listblock[r].width)
+                 else if(redy>=listblock[r].y-radiussmall && redy<=listblock[r].y+listblock[r].height+radiussmall && redx>=listblock[r].x && redx<=listblock[r].x+listblock[r].width)
                  {
                  console.log("game over");	
                   touched = 1;
-                 alert("game over");
-                 break();
-                 document.location.reload();
-                 clearInterval(interval);
+             
                  }
 
                  
-                else 
-                {
-                   if(bluey>=listblock[r].y-radiussmall && bluey<=listblock[r].y+listblock[r].height+radiussmall && bluex>=listblock[r].x && bluex<=listblock[r].x+listblock[r].width)
+                else if(bluey>=listblock[r].y-radiussmall && bluey<=listblock[r].y+listblock[r].height+radiussmall && bluex>=listblock[r].x && bluex<=listblock[r].x+listblock[r].width)
                  {
                  console.log("game over");
                   touched = 1;
-                 alert("game over");
-                 document.location.reload();
-                 clearInterval(interval);
+                
                 }
-               }
-              }*/
-             }          
-            }
 
-         /*  if(touched==1)
-           	{
-           	alert("game over");
-            
- }*/
+                /*else if(Math.sqrt( Math.pow(redx - listblock[r].x, 2) + Math.pow(redy - listblock[r].y, 2) )<=radiussmall)
+                {
+                	touched = 1;
+                } 
+                else if(Math.sqrt( Math.pow(redx - listblock[r].x + listblock[r].width, 2) + Math.pow(redy - listblock[r].y, 2) )<=radiussmall)
+                {
+                	touched = 1;
+                }
+                else if(Math.sqrt( Math.pow(redx - listblock[r].x, 2) + Math.pow(redy - listblock[r].y + listblock[r].height, 2) )<=radiussmall)
+                {
+                	touched = 1;
+                }
+                 else if(Math.sqrt( Math.pow(redx - listblock[r].x + listblock[r].width, 2) + Math.pow(redy - listblock[r].y + listblock[r].height, 2) )<=radiussmall)
+                {
+                	touched = 1;
+                }*/
+       
+       
+           }
+           
+ }
 
  function draw(){
 
@@ -162,23 +167,31 @@ function findsmall(){
             {
             
             	listblock[i].y=sma-250;
-            	listblock[i].width = Math.random()*100;
-            	listblock[i].height = Math.random()*100;
-            	listblock[i].x = canvas.width/2;
+            	listblock[i].width = Math.random()*150;
+            	listblock[i].height = Math.random()*25 + 25;
+            	listblock[i].x = Math.random()*200 + canvas.width/2 - 150;
             	listblock[i].dy = listblock[i].dy+0.001;
+            	++scorecount;
+
             } 
-          
-		        c.fillRect(canvas.width/2, listblock[i].y, listblock[i].width, listblock[i].height);
-		        /*console.log("box "+i+"x: "+listblock[i].x+" y: "+listblock[i].y);
-		        console.log(" ");
-*/
+                c.fillStyle = "black";
+		        c.fillRect(listblock[i].x, listblock[i].y, listblock[i].width, listblock[i].height);
+		      
 		       
                  
         
           }
  }
 
- function incrementy(){
+function score(){
+ 
+c.font = '50px serif';
+c.fillStyle = "black";
+c.fillText("Points scored : " + scorecount, 100, 100);
+ 
+} 
+
+function incrementy(){
 
  	for(var q=0;q<4;++q)
  	{
@@ -189,6 +202,13 @@ function findsmall(){
 
 function animate()
 {       
+        if(touched==1 && repeat==0)
+           	{
+           	alert("game over");
+            play=0;
+            repeat = 1;
+           }
+
 	    if(play==1)
 	   {
 
@@ -233,6 +253,9 @@ function animate()
         findsmall();
 
         draw();
+        
+
+        score();
 
         collisiondetect();
 
@@ -243,3 +266,35 @@ function animate()
 
 
 setInterval(animate, 1);
+
+/*c.beginPath();
+c.fillRect(canvas.width/2-150, 300, 150, 50 );
+
+ c.beginPath();
+		c.strokeStyle = "purple";
+		c.arc(xc, yc, radius, 0, Math.PI*2, false);
+		c.stroke();
+        
+
+
+        c.beginPath();
+        c.save();
+        c.translate(xc, yc);
+        c.rotate(angle);
+
+        c.fillStyle = "red";
+        c.arc(radius, 0, radiussmall, 0, Math.PI*2, false);
+        c.fill();
+       
+
+        c.beginPath();
+        c.fillStyle = "blue";
+        c.arc(-radius, 0, radiussmall, 0, Math.PI*2, false);
+        c.fill();
+
+
+
+        c.restore();
+*/
+
+
